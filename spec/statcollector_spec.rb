61170c440ea0dfab8cf6e3e99bb38ec6d5e351a3
Casper devboxmatsumoto.springer-sbm.com <devboxmatsumoto.springer-sbm.com@springer.com>
2012-08-15 15:52:52 +0100

    Added more validation for Configuration

--- spec/statcollector_spec.rb
+++ spec/statcollector_spec.rb
@@ -3,7 +3,7 @@ require 'psych'
 
 describe "The collector" do
 	before(:all) do
-		@testConfig = Psych.load('
+		@fullconfig = Psych.load('
 name: myproject
 location: /a/location/for/myproject
 max: 10
@@ -11,13 +11,36 @@ collect:
  command1: "a command"
  command2: "another command"
 ')
+
+		@partialconfig = Psych.load('
+name: myproject
+location: /a/location/for/myproject
+collect:
+ command1: "a command"
+')
+
+		@invalidconfig = Psych.load('
+name: myproject
+collect:
+ command1: "a command"
+')
 	end
 
 	it "should be able to be configured" do
-		puts @testConfig["name"]
-		config = StatConfiguration.new(@testConfig)
+		config = StatConfiguration.new(@fullconfig)
 		config.max.should eq(10)
 		config.name.should eq("myproject")
 		config.location.should eq("/a/location/for/myproject")
+	end
+
+	it "should be able to be configured with sensible defaults" do
+		config = StatConfiguration.new(@partialconfig)
+		config.max.should eq(0)
+		config.name.should eq("myproject")
+		config.location.should eq("/a/location/for/myproject")
+	end
+
+	it "should not be able to be configured without a location" do
+		expect { StatConfiguration.new(@invalidconfig) }.to raise_error
 	end
 end
--- statcollector.rb
+++ statcollector.rb
@@ -9,9 +9,10 @@ class StatConfiguration
 	attr_accessor :max, :name, :location
 
 	def initialize(configuration)
-		@max = configuration["max"]
-		@name = configuration["name"]
+		raise "Configuration must include a location" if configuration["location"].nil?
 		@location = configuration["location"]
+		@max = configuration["max"]||=0
+		@name = configuration["name"]
 	end
 end
 
