e515af63890edd44aaa1fc16169b592200a3d6e4
Casper devboxmatsumoto.springer-sbm.com <devboxmatsumoto.springer-sbm.com@springer.com>
2012-08-16 09:20:12 +0100

    StatCollector now has a tmp_dir

--- spec/statcollector_spec.rb
+++ spec/statcollector_spec.rb
@@ -49,3 +49,22 @@ collect:
 		config.collect.should eq "command1" => "a command","command2" => "another command"
 	end
 end
+
+describe "The StatCollector class" do
+	before(:all) do
+		@fullconfig = Psych.load('
+name: myproject
+location: /a/location/for/myproject
+max: 10
+collect:
+ command1: "a command"
+ command2: another command
+')
+	end
+
+	it "should create a temp directory when created" do
+		collector = StatCollector.new(StatConfiguration.new(@fullconfig))
+		collector.tmp_dir.should end_with("myproject")
+		collector.tmp_dir.should start_with("/tmp")
+	end
+end
--- statcollector.rb
+++ statcollector.rb
@@ -2,18 +2,22 @@ require 'tmpdir'
 require 'date'
 
 class StatCollector
+	attr_accessor :tmp_dir
+	def initialize(configuration)
+		@tmp_dir=Dir.mktmpdir + "/" + configuration.name
+	end
 
 end
 
 class StatConfiguration
 	attr_accessor :max, :name, :location, :collect
 
-	def initialize(configuration)
-		raise "Configuration must include a location" if configuration["location"].nil?
-		@location = configuration["location"]
-		@max = configuration["max"]||=0
-		@name = configuration["name"]
-		@collect = configuration["collect"]
+	def initialize(yaml)
+		raise "Configuration must include a location" if yaml["location"].nil?
+		@location = yaml["location"]
+		@max = yaml["max"]||=0
+		@name = yaml["name"]
+		@collect = yaml["collect"]
 
 	end
 end
