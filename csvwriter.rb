ba959a14180bff0dc75b893fd58c4baad42549b2
Casper devboxmatsumoto.springer-sbm.com <devboxmatsumoto.springer-sbm.com@springer.com>
2012-08-17 12:56:11 +0100

    Added initial CSVWriter class and failing test

---
+++ csvwriter.rb
@@ -0,0 +1,4 @@
+require 'csv'
+
+class CSVWriter
+end
---
+++ spec/csvwriter_spec.rb
@@ -0,0 +1,7 @@
+require './csvwriter.rb'
+
+describe "The CSVWriter class" do
+	it "should be able to write a header row" do
+		true.should eq(false)
+	end
+end
