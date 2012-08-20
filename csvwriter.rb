977a55d40f71a736449154366034315ece35fb7f
Michael Hughes <michaelhughes@Michael-Hughess-MacBook-Pro.local>
2012-08-20 15:20:28 +0200

    Tidy up CSVWriter and tests

--- csvwriter.rb
+++ csvwriter.rb
@@ -9,17 +9,20 @@ class CSVWriter
 		title = ["date"] + configuration.collect.map{|key,command|key}
 		stats = [title]
 		commits.each { |commit| stats << 
-			(commit.select{|key,entry|key !=  :hash}.map{|key,entry|entry})
+			(commit.select{|key,entry|key != :hash}.map{|key,entry|entry})
 		}
 		stats.to_json
 	end
 
 	def output_to_csv(configuration,commits)
-		title = (["date"] + configuration.collect.map{|key,command|key}).join(",")
-		stats = [title]
+		stats = [head(commits)]
 		commits.each { |commit| stats << 
-			(commit.select{|key,entry|key !=  :hash}.map{|key,entry|entry}).join(",")
+			(commit.map{|key,entry|entry}).join(",")
 		}
 		stats
+	end
+
+	def head(commits)
+		commits.first.map{|key,entry|"\"#{key}\""}.join(",")
 	end
 end
--- spec/csvwriter_spec.rb
+++ spec/csvwriter_spec.rb
@@ -2,6 +2,8 @@ require './csvwriter.rb'
 
 describe "The CSVWriter class" do
 	it "should be able to write a header row" do
-		true.should eq(false)
+		writer = CSVWriter.new()
+		data = [{:hash => "ahash", :date => "2012-08-15", "command1" => 1, "command2" => 2}]
+		writer.head(data).should eq('"hash","date","command1","command2"')
 	end
 end
