d63b65215d170ded88b47ed0354cb7f2c209119b
Casper devboxmatsumoto.springer-sbm.com <devboxmatsumoto.springer-sbm.com@springer.com>
2012-11-01 11:20:36 +0000

    Added initial case to choose output based on config and tidied up CSVWriter

--- spec/csvwriter_spec.rb
+++ spec/csvwriter_spec.rb
@@ -6,4 +6,10 @@ describe "The CSVWriter class" do
 		data = [{:hash => "ahash", :date => "2012-08-15", "command1" => 1, "command2" => 2}]
 		writer.head(data).should eq('"hash","date","command1","command2"')
 	end
+
+	it "should be able to generate csv output" do
+		writer = CSVWriter.new()
+		data = [{:hash => "ahash", :date => "2012-08-15", "command1" => 1, "command2" => 2},{:hash => "bhash", :date => "2012-08-16", "command1" => 3, "command2" => 4}]
+		writer.output(data).should eq("\"hash\",\"date\",\"command1\",\"command2\"\n\"ahash\",\"2012-08-15\",\"1\",\"2\"\n\"bhash\",\"2012-08-16\",\"3\",\"4\"")
+	end
 end
