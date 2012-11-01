37b1b93556932c029bc2fdf557356377f6cb791b
Casper devboxmatsumoto.springer-sbm.com <devboxmatsumoto.springer-sbm.com@springer.com>
2012-11-01 11:33:09 +0000

    JSONWriter can now output either a json object or a json array

--- jsonwriter.rb
+++ jsonwriter.rb
@@ -4,7 +4,11 @@ class JSONWriter
 	def initialize()
 	end
 
-	def output(configuration,commits)
+	def output_json(configuration,commits)
+		commits.to_json
+	end
+
+	def output_array(configuration,commits)
 		title = ["date"] + configuration.collect.map{|key,command|key}
 		stats = [title]
 		commits.each { |commit| stats <<
