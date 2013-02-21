a344ad58704ecc26558fcdc1bd9c3b6bda1e336d
Michael Hughes <mikeyhu@Michaels-MacBook-Pro.local>
2013-02-21 16:46:52 +0000

    Added casper config files

---
+++ run-casper.sh
@@ -0,0 +1,11 @@
+./multigraph.rb\
+ repositories/casper-aim.yaml\
+ repositories/casper-platform.yaml\
+ repositories/casper-core.yaml\
+ repositories/casper-scheduler.yaml\
+ repositories/casper-identity.yaml\
+ repositories/casper-sid.yaml\
+ repositories/casper-mud.yaml\
+ repositories/casper-track.yaml\
+ repositories/casper-mum.yaml\
+ > casper-graphs.html
